adac903e88273f6efb1e1f7b1e974d09f4bb85ee
Gonzalo <gptasis@yahoo.es>
2019-09-11 12:44:09 +0200

    new changes

--- dudas/index.js
+++ dudas/index.js
@@ -1,50 +1,60 @@
-// EJERCICIOS QUE NECESITO QUE REVISEMOS JUNTOS.
-// CIERTAS FORMAS DE COMO ESTAN HECHOS ME GENERAN DUDAS
+// EXAMPLE TEST CASES
+//input: "Hello-LOL"
+//output: hELLO-lol
 
+function SwapCase(str) { 
 
+  // turn the string into an array of characters
+  var chars = str.split('');
+  
+  // loop through the array swapping letter cases
+  // uppercase -> lowercase
+  // lowercase -> uppercase
+  for (var i = 0; i < chars.length; i++) {
+    if (chars[i] === chars[i].toUpperCase()) { 
+      chars[i] = chars[i].toLowerCase();
+    }
+    else if (chars[i] === chars[i].toLowerCase()) { 
+      chars[i] = chars[i].toUpperCase();
+    }
+  }
+  
+  // return the modified array of characters joined as a string
+  return chars.join('');
+         
+}
+   
+SwapCase("Hello World");  
 
-const inventors = [
-    { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
-    { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
-    { first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 },
-    { first: 'Marie', last: 'Curie', year: 1867, passed: 1934 },
-    { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 },
-    { first: 'Nicolaus', last: 'Copernicus', year: 1473, passed: 1543 },
-    { first: 'Max', last: 'Planck', year: 1858, passed: 1947 },
-    { first: 'Katherine', last: 'Blodgett', year: 1898, passed: 1979 },
-    { first: 'Ada', last: 'Lovelace', year: 1815, passed: 1852 },
-    { first: 'Sarah E.', last: 'Goode', year: 1855, passed: 1905 },
-    { first: 'Lise', last: 'Meitner', year: 1878, passed: 1968 },
-    { first: 'Hanna', last: 'Hammarström', year: 1829, passed: 1909 }
-  ];
+function SwapCase(str) { 
+  for(var i=0; i< str.length;i++){
+    if(str[i].match(/[a-z]/i) !==null) {
+      str[i].toUpperCase();
+    }if(str[i].match(/[A-Z]/i) !==null){
+      str[i].toLowerCase();
+    }
+  }
+  SwapCase("Hello-LOL");
 
-  const people = ['Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick', 'Beecher, Henry', 'Beethoven, Ludwig', 'Begin, Menachem', 'Belloc, Hilaire', 'Bellow, Saul', 'Benchley, Robert', 'Benenson, Peter', 'Ben-Gurion, David', 'Benjamin, Walter', 'Benn, Tony', 'Bennington, Chester', 'Benson, Leana', 'Bent, Silas', 'Bentsen, Lloyd', 'Berger, Ric', 'Bergman, Ingmar', 'Berio, Luciano', 'Berle, Milton', 'Berlin, Irving', 'Berne, Eric', 'Bernhard, Sandra', 'Berra, Yogi', 'Berry, Halle', 'Berry, Wendell', 'Bethea, Erin', 'Bevan, Aneurin', 'Bevel, Ken', 'Biden, Joseph', 'Bierce, Ambrose', 'Biko, Steve', 'Billings, Josh', 'Biondo, Frank', 'Birrell, Augustine', 'Black Elk', 'Blair, Robert', 'Blair, Tony', 'Blake, William'];
+  function SwapCase(str) { 
+    return str.replace(/\b[a-z]/gi,function(char){
+      return char.toUpperCase();
+    });
+    return str.replace(/\b[A-Z]/gi,function(char){
+      return char.toLowerCase();
+    });
+
+    }
+    SwapCase("Hello-LOL");
+
 
  
 
-   Array.prototype.sort()
-// 3. Sort the inventors by birthdate, oldest to youngest
+   
 
-     const ordered = inventors.sort(function(a, b) {
-     inventors   
-      if(a.year > b.year) {
-        return 1;
-       } else {
-         return -1;
-       }
-    });
+   
 
-    const ordered1 = inventors.sort((a, b) => a.year > b.year ? 1 : -1);
-    console.table(ordered1);
-
-
-    // 5. Sort the inventors by years lived
-    const oldest = inventors.sort(function(a, b) {
-        const lastInventor = a.passed - a.year;
-        const nextInventor = b.passed - b.year;
-        return lastInventor > nextInventor ? -1 : 1;
-      });
-      console.table(oldest);
+   
 
       // 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
     // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
