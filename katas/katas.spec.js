ced68e18d9158d52022fede9f127f0c7fe80655e
Gonzalo <gptasis@yahoo.es>
2019-09-21 19:52:04 +0200

    commit

--- katas/katas.spec.js
+++ katas/katas.spec.js
@@ -5,27 +5,3 @@ Passwords must abide by the following requirements:
 
 More than 3 characters but less than 20.
 
-Must contain only alphanumeric characters.
-
-Must contain letters and numbers. */
-const passwordValidator = require('./katas');
-
-describe.skip('Password Validator', () => {
-  it('Must be between 3 an 20 characters', () => {
-    expect(passwordValidator('u1')).toBe('INVALID');
-    expect(passwordValidator('ThisPasswordIsTooLong1234')).toBe('INVALID');
-  });
-  it('Username123 to be valid', () => {
-    expect(passwordValidator('Username123')).toBe(
-      'VALID',
-    );
-  });
-
-  it('Must contain only alphanumeric characters', () => {
-    expect(passwordValidator('Username123!')).toBe('INVALID');
-  });
-  it('Must contain letters and numbers', () => {
-    expect(passwordValidator('Username')).toBe('INVALID');
-    expect(passwordValidator('123')).toBe('INVALID');
-  });
-});
