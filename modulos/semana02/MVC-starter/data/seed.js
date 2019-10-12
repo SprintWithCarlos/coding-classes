4d5165e92791a85c815eabf64e0c6941c483c1f3
Carlos Marchena <sprintwithcarlos@pm.me>
2019-10-12 20:43:43 +0200

    Semana 04: Clase Módulos

--- modulos/semana02/MVC-starter/data/seed.js
+++ modulos/semana02/MVC-starter/data/seed.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-plusplus */
-const faker = require("faker");
-const fs = require("fs");
+const faker = require('faker');
+const fs = require('fs');
 
 const users = [];
 const posts = [];
@@ -13,7 +13,7 @@ for (let i = 0; i < 7; i++) {
     email: faker.internet.email(),
     title: faker.company.bsNoun(),
     id: i + 1,
-    description: faker.lorem.paragraphs(5)
+    description: faker.lorem.paragraphs(5),
   };
   users.push(user);
 }
@@ -28,32 +28,32 @@ class User {
   }
 }
 
-fs.writeFile("users.json", JSON.stringify(users), err => {
+fs.writeFile('users.json', JSON.stringify(users), (err) => {
   if (err) throw console.error(err);
-  console.log("The file was saved!");
+  console.log('The file was saved!');
 });
 
-for (let i = 0; i < 30; i++) {
+for (let i = 0; i < 100; i++) {
   const post = {
     id: i + 1,
-    date: faker.date.recent().toLocaleDateString("es-ES", {
-      weekday: "long",
-      hour: "numeric",
-      minute: "numeric",
-      seconds: "numeric",
-      day: "numeric",
-      month: "long",
-      year: "numeric"
+    date: faker.date.recent().toLocaleDateString('es-ES', {
+      weekday: 'long',
+      hour: 'numeric',
+      minute: 'numeric',
+      seconds: 'numeric',
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric',
     }),
     title: faker.lorem.sentence(),
     body: faker.lorem.paragraphs(5),
     category: faker.hacker.noun(),
     image: faker.image.image(),
-    author: new User()
+    author: new User(),
   };
   posts.push(post);
 }
-fs.writeFile("posts.json", JSON.stringify(posts), err => {
+fs.writeFile('posts.json', JSON.stringify(posts), (err) => {
   if (err) throw console.error(err);
-  console.log("The file was saved!");
+  console.log('The file was saved!');
 });
