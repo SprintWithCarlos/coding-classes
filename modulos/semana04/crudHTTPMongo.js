043778dd448c5b0ea108b064d0a9244d56472534
Carlos Marchena <sprintwithcarlos@pm.me>
2019-10-13 11:36:12 +0200

    Starting Mongo

--- modulos/semana04/crudHTTPMongo.js
+++ modulos/semana04/crudHTTPMongo.js
@@ -9,9 +9,13 @@
 const express = require('express');
 
 const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true });
+
+
 const morgan = require('morgan');
 
-const mysqlConnection = require('./database');
 
 // Middlewares
 app.use(express.json());
